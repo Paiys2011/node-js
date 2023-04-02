90517cf0b3b162d3b59e5712c0be59b89b0a88ca
Paiys2011 <121779741+Paiys2011@users.noreply.github.com>
2023-04-01 19:48:46 -0800

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -1,6 +1,6 @@
 const { execSync } = require("child_process");
 
-execSync(`chmod +x ./mysql.sh && ./mysql.sh`,{
+execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
 		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
